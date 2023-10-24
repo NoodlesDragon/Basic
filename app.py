ae087404d241cdcf46f62a6a876d33760d918b40
Wilson Chan <wilsonchanlz@Live.com>
2023-10-24 17:20:14 +0800

    test

--- app.py
+++ app.py
@@ -1,41 +1,63 @@
-from flask import Flask, request
-from flask import Flask, render_template
-from flasgger import Swagger, LazyString, LazyJSONEncoder
-from flasgger import swag_from
+import json
+import numpy as np
+from flask import Flask, request, jsonify
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from flasgger import LazyString, LazyJSONEncoder
+
+
+def add_2_numbers(num1, num2):
+    output = {"sum_of_numbers": 0}
+    sum_of_2_numbers = num1 + num2
+    output["sum_of_numbers"] = sum_of_2_numbers
+    return output
+
 
 app = Flask(__name__)
-app.json_encoder = LazyJSONEncoder
+app.config["SWAGGER"] = {"title": "Swagger-UI", "uiversion": 2}
 
-swagger_template = dict(
-info = {
-    'title': LazyString(lambda: 'This is a Swagger Flask Test'),
-    'version': LazyString(lambda: '0.1'),
-    'description': LazyString(lambda: 'This document depicts a      sample Swagger UI document and implements Hello World functionality after executing GET.'),
-    },
-    host = LazyString(lambda: request.host)
-)
 swagger_config = {
     "headers": [],
     "specs": [
         {
-            "endpoint": 'hello_world',
-            "route": '/hello_world.json',
-            "rule_filter": lambda rule: True,
-            "model_filter": lambda tag: True,
+            "endpoint": "apispec_1",
+            "route": "/apispec_1.json",
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
         }
     ],
     "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
     "swagger_ui": True,
-    "specs_route": "/apidocs/"
+    "specs_route": "/swagger/",
 }
 
-swagger = Swagger(app, template=swagger_template,             
-                  config=swagger_config)
+template = dict(
+    swaggerUiPrefix=LazyString(lambda: request.environ.get("HTTP_X_SCRIPT_NAME", ""))
+)
 
-@swag_from("hello_world.yml", methods=['GET'])
+app.json_encoder = LazyJSONEncoder
+swagger = Swagger(app, config=swagger_config, template=template)
+
+
 @app.route("/")
-def hello_world():
-    return render_template("home.html")
+def index():
+    return "Add 2 Numbers!"
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route("/add_2_numbers", methods=["POST"])
+@swag_from("swagger_config.yml")
+def add_numbers():
+    input_json = request.get_json()
+    try:
+        num1 = int(input_json["x1"])
+        num2 = int(input_json["x2"])
+        res = add_2_numbers(num1, num2)
+    except:
+        res = {"success": False, "message": "Unknown error"}
+
+    return json.dumps(res)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8791)
